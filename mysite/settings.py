a58a0efe5f4bf2abf9bd2eb248759be66a28ed07
Tester <tester@gmail.com>
2022-07-15 18:34:59 +0530

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #    }
 #}##
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
